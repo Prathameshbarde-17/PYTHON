789c50e88ad9d8f6ef2c1ab720fc0bfccf348b6a
Prathamesh Barde <167535391+Prathameshbarde-17@users.noreply.github.com>
2025-12-16 23:29:17 +0530

    add file
    add

---
+++ 1stProgram.py
@@ -0,0 +1,53 @@
+print("I am Coder ")
+print("Starting my Python journey")
+
+#Variables
+
+name = "Prathamesh"
+age = 22
+qualification ="B.E"
+
+print("my name is:",name, "my age is:",age)
+
+print("and qualification is:",qualification)      
+
+# Data Types 
+
+name ="Patty"
+age =22
+cgpa = 10.0
+isGraduated = True
+print (type(name))
+print (type(age))
+print (type(cgpa))
+print(type(isGraduated))
+
+# Algebric Operations 
+# SUM
+
+a =10
+b =20
+sum= a+b
+print(sum)
+
+#Subraction
+subtract = b-a
+print(subtract)
+
+#Multification
+multiply= a*b
+print(multiply)
+
+#Division
+divide = b/a
+print(divide)
+
+#Modulus
+
+module=b%a
+print(module)
+
+#Exponent 
+exponent =a**2
+print(exponent)
+
