7a69b70dd8c59d9d75edfe2846adc331dee71e5b
Prathamesh Barde <167535391+Prathameshbarde-17@users.noreply.github.com>
2026-01-06 23:18:34 +0530

    Add OOP python file

---
+++ OOPs/OOPS1_Program.py
@@ -0,0 +1,50 @@
+'''OOPS IN PYTHON'''
+
+#Encapsulation -->
+              # Encapsulation is a wrapping data and functions into single unit
+                  
+
+#Abstraction -->
+            # Abstraction is Hididng implementation details of a class 
+            # and showing only neccesary details to user.
+            
+#Inheritance -->
+            # Inheritance is a way to create a new class from an existing class
+
+#Polymorphism -->
+            # Polymorphism is a way to create a new class from an existing class
+
+
+#Data Hiding -->
+            # Data Hiding is a way to hide implementation details of a class
+
+
+#Method Overloading ->
+            # Method Overloading is a way to create a new class from an existing class
+
+
+#Method Overriding -->
+            # Method Overriding is a way to create a new class from an existing class
+
+
+#Constructor -->
+            # Constructor is a way to create a new class from an existing class
+
+
+#Destructor -->
+            # Destructor is a way to create a new class from an existing class
+
+#Super -->
+            # Super is a way to create a new class from an existing class
+
+#Self -->
+            # Self is a way to create a new class from an existing class
+
+#Static --> 
+            # Static is a way to create a new class from an existing class
+
+#Abstract -->
+            # Abstract is a way to create a new class from an existing class
+
+
+            
