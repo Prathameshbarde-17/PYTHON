abcc71b565031c20f9ef6cccbe67ce47bb6477bd
Prathamesh Barde <167535391+Prathameshbarde-17@users.noreply.github.com>
2026-01-08 22:55:34 +0530

    Update 3.py

--- All_Questions/Loops/3.py
+++ All_Questions/Loops/3.py
@@ -1,1 +1,12 @@
+"""
+# Write a Python program to guess a number between 1 and 9.
 
+Note : User is prompted to enter a guess. If the user guesses wrong then the prompt appears again until the guess is correct, on successful guess, user will get a "Well guessed!" message, and the program will exit.
+"""
+
+"""
+Write a Python program that randomly selects a number between 1 and 9 and repeatedly prompts the user until the correct guess is made.
+Write a Python program to implement a number guessing game with a maximum number of attempts and hints for higher or lower.
+Write a Python program to create a guessing game where the program gives feedback on how close the guess is to the target.
+Write a Python program that logs the number of guesses made and congratulates the user once the correct number is guessed.
+"""
