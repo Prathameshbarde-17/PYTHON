dad9fcd8c768abef74be59abe7ff3bcd37bcf836
Prathamesh Barde <167535391+Prathameshbarde-17@users.noreply.github.com>
2026-01-08 22:55:00 +0530

    Update 2.py

--- All_Questions/Loops/2.py
+++ All_Questions/Loops/2.py
@@ -1,1 +1,24 @@
 
+"""
+#
+2. Temperature Converter
+
+Write a Python program to convert temperatures to and from Celsius and Fahrenheit.
+
+[ Formula : c/5 = f-32/9 [ where c = temperature in celsius and f = temperature in fahrenheit ]
+
+Expected Output :
+
+60°C is 140 in Fahrenheit
+45°F is 7 in Celsius
+
+"""
+temp = int(input("Enter temperature: "))
+
+fahrenheit = (temp * 9/5)+32
+
+celsius = (temp-32)*5/9
+
+print(f"{temp}°F is {celsius} in Celsius\n{temp}°C is ")
+print(f"{fahrenheit} in Fahrenheit")
+
