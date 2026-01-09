d71001ba421aa0f1b90c1820b5217d503ce51b56
Prathamesh Barde <167535391+Prathameshbarde-17@users.noreply.github.com>
2026-01-09 22:52:08 +0530

    Update 7.py

--- All_Questions/Loops/7.py
+++ All_Questions/Loops/7.py
@@ -1,1 +1,29 @@
 
+
+
+
+"""
+
+7. Print Items with Types
+
+# Write a Python program that prints each item and its corresponding type from the following list.
+
+Sample List : datalist = [1452, 11.23, 1+2j, True, 'w3resource', (0, -1), [5, 12], {"class":'V', "section":'A'}]
+
+"""
+
+datalist = [1452, 11.23, 1+2j, True, 'w3resource', (0, -1), [5, 12], {"class":'V', "section":'A'}]
+
+for item in datalist:
+    print(item, type(item))
+
+
+
+"""
+# Questions 
+Write a Python program to iterate through a heterogeneous list and print each element along with its data type.
+Write a Python program to use a for loop and type() function to display the type of each item in a mixed-type list.
+Write a Python program to create a formatted string that lists each element of a list and its corresponding type.
+Write a Python program to use list comprehension to produce a list of tuples where each tuple contains an element and its type
+"""
+
