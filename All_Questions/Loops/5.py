53c5e63ceffa583976a588d2b8b633fafab16689
Prathamesh Barde <167535391+Prathameshbarde-17@users.noreply.github.com>
2026-01-09 22:50:15 +0530

    Update 5.py

--- All_Questions/Loops/5.py
+++ All_Questions/Loops/5.py
@@ -1,1 +1,38 @@
 
+
+
+"""
+
+5. Reverse a Word
+
+Write a Python program that accepts a word from the user and reverses it.
+
+"""
+"""
+# Solution 1
+word = input("Enter a word: ")
+
+reversed_word = word[ ::-1 ]
+
+print("Reversed word:", reversed_word)
+
+"""
+
+#Solution 2
+
+word = input("Enter a word: ")
+
+reverse_Word=" "
+
+for i in range(len(word)-1,-1,-1):
+    reverse_Word += word[i]
+
+print("Reversed word:", reverse_Word)
+
+
+"""
+#Questions
+Write a Python program to reverse a word entered by the user using slicing.
+Write a Python program to reverse each word in a sentence while maintaining the word order.
+Write a Python program to recursively reverse a given string without using slicing.
+Write a Python program to reverse a word using a stack data structure to simulate character popping.
